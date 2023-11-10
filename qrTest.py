2f152ae044562dd3938118f7d50fc15c997e2324
Berdehacks <angelberdejad@gmail.com>
2023-11-10 15:40:01 -0600

    push de qrs

--- qrTest.py
+++ qrTest.py
@@ -12,29 +12,28 @@ while (True):
     # Capture the video frame
     # by frame
     ret, frame = vid.read()
-    ret_qr, points = qr.detect(frame)
+    ret_qr, points = qr.detectMulti(frame)
 
     print(points)
 
-    for corner in points[0]:
+    if points is not None:
+        for point in points:
+            for corner in point:
+                center_coordinates = (int(corner[0]), int(corner[1]))
 
-        frame = cv2.circle(frame, center_coordinates, radius, color, thickness)
+                frame = cv2.circle(
+                    frame, center_coordinates, 4, (255, 0, 0), 1)
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
+            # Display the resulting frame
+            cv2.imshow('frame', frame)
 
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            # the 'q' button is set as the
+            # quitting button you may use any
+            # desired button of your choice
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
 # After the loop release the cap object
 vid.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
-
-[[[ 427.        348.      ]
-  [ 208.        368.      ]
-  [ 111.624916 -107.36884 ]
-  [ 637.        102.      ]]]
