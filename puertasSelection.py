9902bd42212aace79048c2448a9ca8351b62bf4d
Berdehacks <angelberdejad@gmail.com>
2023-11-11 00:52:50 -0600

    ROI detection

--- puertasSelection.py
+++ puertasSelection.py
@@ -1,19 +1,50 @@
 import shapeRecognition
 import numpy as np
+import cv2
 
 
-p1 = (209, 299)
-p2 = (1306, 799)
-p3 = (293, 759)
-p4 = (1398, 300)
+img = cv2.imread("prueba_2.jpg")
+
+p1 = np.array([293, 759])
+p2 = np.array([1306,  799])
+p3 = np.array([209, 299])
+p4 = np.array([1398, 300])
+
+Q1 = p2 - p1
+Q2 = p4 - p3
+
+normalized_q1 = Q1 / np.linalg.norm(Q1)
+normalized_q2 = Q2 / np.linalg.norm(Q2)
+print(normalized_q1)
+print(normalized_q2)
+
+p = 0.3
+
+shorten_Q1 = normalized_q1 * p  # adjust percentage of horizontal roi
+shorten_Q2 = normalized_q2 * p  # adjust percentage of horizontal roi
+
+PQ1 = p1 + shorten_Q1
+PQ2 = p3 + shorten_Q2
+
+PQ1Y = PQ1[1]
+PQ2Y = PQ2[1]
+
+p1y = p1[1]
+p3y = p3[1]
+
+print(p1, p2, PQ1)
+
+minY = min(PQ2Y, p3y)
+maxY = max(PQ1Y, p1y)
+
+print(minY, maxY, p1[0], int(PQ2[0]))
 
 
-def selction(img, p1, p2, p3, p4):
+img = img[p3[1]:p1[1], p1[0]:p1[0] + int(PQ1[0])]
+# img = img[int(maxY):minY, int(PQ2[0]):p1[0]]
 
-    Q1 = p1 - p2
-    Q2 = p3 - p4
+shape = shapeRecognition.shapeRecognition(img)
+print(shape)
 
-    normalized_q1 = Q1 / np.linalg.norm(Q1)
-    normalized_q2 = Q1 / np.linalg.norm(Q2)
-
-    return
+cv2.imshow("roi", img)
+cv2.waitKey(0)
--- shapeRecognition.py
+++ shapeRecognition.py
@@ -3,15 +3,17 @@ import cv2
 import numpy as np
 
 
-def shapeRecognition(img, inputRoiP1, inputRoiP2):
+def shapeRecognition(img):
 
-    img = img[inputRoiP1[1]: inputRoiP2[1], inputRoiP1[0]: inputRoiP2[0]]
+    # img = img[inputRoiP1[1]: inputRoiP2[1], inputRoiP1[0]: inputRoiP2[0]]
 
     # converting image into grayscale imag
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # setting threshold of gray image
     _, threshold = cv2.threshold(gray, 140, 180, cv2.THRESH_OTSU)
+
+    cv2.imshow("roi", threshold)
 
     # threshold = cv2.bitwise_not(threshold)
 
@@ -22,7 +24,7 @@ def shapeRecognition(img, inputRoiP1, inputRoiP2):
     for contour in contours[1:]:
         # cv2.approxPloyDP() function to approximate the shape
         approx = cv2.approxPolyDP(
-            contour, 0.05 * cv2.arcLength(contour, True), True)
+            contour, 0.01 * cv2.arcLength(contour, True), True)
 
     # using drawContours() function
 
